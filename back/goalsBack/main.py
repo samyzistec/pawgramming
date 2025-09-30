69a2ad2488b18167c9b85079e2530ff04bc070dd
Samuel Perez Zistecatl <samyzistec@gmail.com>
2025-09-30 00:31:17 -0600

    Add: Se agregan metodos para pruebas
    Se agregan clases para la prueba del arquetipo

--- back/goalsBack/main.py
+++ back/goalsBack/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
-
+from goalsBack.services.defending_corners import analyze_corners
+from goalsBack.services.players import get_player_stats
+from goalsBack.services.matches import get_match_events
 app = FastAPI()
 
 
@@ -11,3 +13,19 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+# Nuevo endpoint
+@app.post("/recommendations/corners")
+async def corners_recommendations(events: list[dict]):
+    result = analyze_corners(events)
+    return result
+
+#  Nuevo: Endpoint de jugadores
+@app.get("/players/{player_id}/stats")
+async def player_stats(player_id: int):
+    return get_player_stats(player_id)
+
+#  Nuevo: Endpoint de partidos
+@app.get("/matches/{match_id}/events")
+async def match_events(match_id: int):
+    return get_match_events(match_id)
